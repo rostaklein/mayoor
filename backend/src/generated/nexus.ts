94e546e2d20db1f3e2bc75659a14559e24123eee
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-02-07 00:36:02 +0100

    first prisma db migration done, basic gql schema added

--- backend/src/generated/nexus.ts
+++ backend/src/generated/nexus.ts
@@ -3,12 +3,15 @@
  * Do not make changes to this file directly
  */
 
-import * as Context from '../context';
+import * as Context from "../context"
+import * as prisma from "@prisma/client"
+
+
 
 declare global {
   interface NexusGenCustomOutputProperties<TypeName extends string> {
-    crud: NexusPrisma<TypeName, 'crud'>;
-    model: NexusPrisma<TypeName, 'model'>;
+    crud: NexusPrisma<TypeName, 'crud'>
+    model: NexusPrisma<TypeName, 'model'>
   }
 }
 
@@ -16,35 +19,53 @@ declare global {
   interface NexusGen extends NexusGenTypes {}
 }
 
-export interface NexusGenInputs {}
+export interface NexusGenInputs {
+}
 
-export interface NexusGenEnums {}
+export interface NexusGenEnums {
+}
 
 export interface NexusGenRootTypes {
+  Order: prisma.Order;
   Query: {};
+  User: prisma.User;
   String: string;
   Int: number;
   Float: number;
   Boolean: boolean;
   ID: string;
+  DateTime: any;
 }
 
-export interface NexusGenAllTypes extends NexusGenRootTypes {}
+export interface NexusGenAllTypes extends NexusGenRootTypes {
+}
 
 export interface NexusGenFieldTypes {
-  Query: {
-    // field return type
-    ok: boolean; // Boolean!
-  };
+  Order: { // field return type
+    createdAt: any; // DateTime!
+    createdBy: NexusGenRootTypes['User']; // User!
+    number: number; // Int!
+    updatedAt: any; // DateTime!
+  }
+  Query: { // field return type
+    getAllOrders: NexusGenRootTypes['Order'][]; // [Order!]!
+  }
+  User: { // field return type
+    email: string; // String!
+    id: string; // String!
+    name: string | null; // String
+  }
 }
 
-export interface NexusGenArgTypes {}
+export interface NexusGenArgTypes {
+}
 
-export interface NexusGenAbstractResolveReturnTypes {}
+export interface NexusGenAbstractResolveReturnTypes {
+}
 
 export interface NexusGenInheritedFields {}
 
-export type NexusGenObjectNames = 'Query';
+export type NexusGenObjectNames = "Order" | "Query" | "User";
 
 export type NexusGenInputNames = never;
 
@@ -52,7 +73,7 @@ export type NexusGenEnumNames = never;
 
 export type NexusGenInterfaceNames = never;
 
-export type NexusGenScalarNames = 'Boolean' | 'Float' | 'ID' | 'Int' | 'String';
+export type NexusGenScalarNames = "Boolean" | "DateTime" | "Float" | "ID" | "Int" | "String";
 
 export type NexusGenUnionNames = never;
 
@@ -70,28 +91,19 @@ export interface NexusGenTypes {
   interfaceNames: NexusGenInterfaceNames;
   scalarNames: NexusGenScalarNames;
   unionNames: NexusGenUnionNames;
-  allInputTypes:
-    | NexusGenTypes['inputNames']
-    | NexusGenTypes['enumNames']
-    | NexusGenTypes['scalarNames'];
-  allOutputTypes:
-    | NexusGenTypes['objectNames']
-    | NexusGenTypes['enumNames']
-    | NexusGenTypes['unionNames']
-    | NexusGenTypes['interfaceNames']
-    | NexusGenTypes['scalarNames'];
-  allNamedTypes:
-    | NexusGenTypes['allInputTypes']
-    | NexusGenTypes['allOutputTypes'];
+  allInputTypes: NexusGenTypes['inputNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['scalarNames'];
+  allOutputTypes: NexusGenTypes['objectNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['unionNames'] | NexusGenTypes['interfaceNames'] | NexusGenTypes['scalarNames'];
+  allNamedTypes: NexusGenTypes['allInputTypes'] | NexusGenTypes['allOutputTypes']
   abstractTypes: NexusGenTypes['interfaceNames'] | NexusGenTypes['unionNames'];
   abstractResolveReturn: NexusGenAbstractResolveReturnTypes;
 }
 
+
 declare global {
-  interface NexusGenPluginTypeConfig<TypeName extends string> {}
-  interface NexusGenPluginFieldConfig<
-    TypeName extends string,
-    FieldName extends string
-  > {}
-  interface NexusGenPluginSchemaConfig {}
+  interface NexusGenPluginTypeConfig<TypeName extends string> {
+  }
+  interface NexusGenPluginFieldConfig<TypeName extends string, FieldName extends string> {
+  }
+  interface NexusGenPluginSchemaConfig {
+  }
 }
--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,8 +1,39 @@
 import { nexusPrismaPlugin } from 'nexus-prisma';
-import { makeSchema } from 'nexus';
+import { makeSchema, objectType } from 'nexus';
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id();
+    t.model.name();
+    t.model.email();
+  },
+});
+
+const Order = objectType({
+  name: 'Order',
+  definition(t) {
+    t.model.number();
+    t.model.createdBy();
+    t.model.createdAt();
+    t.model.updatedAt();
+  },
+});
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.list.field('getAllOrders', {
+      type: 'Order',
+      resolve: (_parent, _args, ctx) => {
+        return ctx.prisma.order.findMany();
+      },
+    });
+  },
+});
 
 export const schema = makeSchema({
-  types: [],
+  types: [Query, User, Order],
   plugins: [nexusPrismaPlugin()],
   outputs: {
     schema: __dirname + '/generated/schema.graphql',
