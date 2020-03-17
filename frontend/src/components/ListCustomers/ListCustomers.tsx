5204529750ac6f3165c083df30f336827050b2c7
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-03-17 23:54:48 +0100

    paginated table extracted from listcustomers

--- frontend/src/components/ListCustomers/ListCustomers.tsx
+++ frontend/src/components/ListCustomers/ListCustomers.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable  @typescript-eslint/camelcase */
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { RightCircleOutlined } from '@ant-design/icons';
-import { Table, Row, Col, Empty, Button } from 'antd';
+import { Button } from 'antd';
 import { useQuery } from 'react-apollo';
 import { TFunction } from 'i18next';
 import { ColumnProps } from 'antd/lib/table';
 import { Link } from 'react-router-dom';
-import { PaginationConfig } from 'antd/lib/pagination';
 
 import {
 	GetAllCustomers,
@@ -15,7 +14,7 @@ import {
 	GetAllCustomers_getAllCustomers_items,
 } from '../../__generated__/types';
 import { PageTitle } from '../MainWrapper/MainWrapper.styles';
-import { StyledTableWrapper, StyledSearch } from '../SharedStyles/Table.styles';
+import { PaginatedTable } from '../PaginatedTable/PaginatedTable';
 
 import { GET_ALL_CUSTOMERS_QUERY } from './queries';
 
@@ -66,8 +65,7 @@ const getColumns = (t: TFunction): ColumnProps<GetAllCustomers_getAllCustomers_i
 
 export const ListCustomers: React.FC = () => {
 	const { t } = useTranslation();
-	const [currentPageNumber, setCurrentPageNumber] = useState(1);
-	const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
 	const { data, loading, fetchMore, refetch } = useQuery<
 		GetAllCustomers,
 		GetAllCustomersVariables
@@ -76,83 +74,39 @@ export const ListCustomers: React.FC = () => {
 		fetchPolicy: 'network-only',
 	});
 
-	const items = data?.getAllCustomers.items ?? [];
-
-	const pagination: PaginationConfig = {
-		current: currentPageNumber,
-		total: data?.getAllCustomers.totalCount,
-		pageSize: PAGE_SIZE,
-		hideOnSinglePage: true,
-		showTotal: (total, range) =>
-			t('Showing {{start}}-{{end}} of {{total}} customers', {
-				total,
-				start: range[0],
-				end: range[1],
-			}),
-		onChange: (newPageNumber) => {
-			setCurrentPageNumber(newPageNumber);
-			fetchMore({
-				variables: { first: PAGE_SIZE, skip: (newPageNumber - 1) * PAGE_SIZE },
-				updateQuery: (_, { fetchMoreResult }) => {
-					if (!fetchMoreResult) {
-						throw new Error('Failed to load more');
-					}
-					return fetchMoreResult;
-				},
-			});
-		},
-	};
-
-	const selectHandler = (id: string, isSelected: boolean) => {
-		if (isSelected) {
-			return setSelectedItems([...selectedItems, id]);
-		}
-		setSelectedItems(selectedItems.filter((currentId) => currentId !== id));
-	};
-
-	const searchSubmitHandler = (searchValue: string) => {
-		setCurrentPageNumber(1);
+	const searchHandler = (searchValue: string) => {
 		refetch({ first: PAGE_SIZE, search: searchValue });
 	};
+
+	const paginationChangedHandler = (newPageNumber: number) => {
+		fetchMore({
+			variables: { first: PAGE_SIZE, skip: (newPageNumber - 1) * PAGE_SIZE },
+			updateQuery: (_, { fetchMoreResult }) => {
+				if (!fetchMoreResult) {
+					throw new Error('Failed to load more');
+				}
+				return fetchMoreResult;
+			},
+		});
+	};
+
+	const items = data?.getAllCustomers.items ?? [];
 
 	return (
 		<>
 			<PageTitle>{t('Customers')}</PageTitle>
-			<StyledTableWrapper>
-				<Row justify="end">
-					<Col xs={24} md={6}>
-						<StyledSearch
-							enterButton
-							placeholder={t('Search customers')}
-							onSearch={searchSubmitHandler}
-						/>
-					</Col>
-				</Row>
-
-				<Table<GetAllCustomers_getAllCustomers_items>
-					columns={getColumns(t)}
-					dataSource={items}
-					pagination={pagination}
-					loading={loading}
-					rowKey={(record) => record.id}
-					size="middle"
-					rowSelection={{
-						onSelect: ({ id }, isSelected) => selectHandler(id, isSelected),
-						selectedRowKeys: selectedItems,
-						onSelectAll: (_, selectedRows) => {
-							setSelectedItems(selectedRows.map(({ id }) => id));
-						},
-					}}
-					locale={{
-						emptyText: (
-							<Empty
-								image={Empty.PRESENTED_IMAGE_SIMPLE}
-								description={t('Customers list is empty')}
-							/>
-						),
-					}}
-				/>
-			</StyledTableWrapper>
+			<PaginatedTable<GetAllCustomers_getAllCustomers_items>
+				columns={getColumns(t)}
+				records={items}
+				totalCount={data?.getAllCustomers.totalCount ?? 0}
+				onPaginationChange={paginationChangedHandler}
+				onSearch={searchHandler}
+				loading={loading}
+				translations={{
+					emptyResult: t('Customers list is empty'),
+					search: t('Search customers'),
+				}}
+			/>
 		</>
 	);
 };
---
+++ frontend/src/components/PaginatedTable/PaginatedTable.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Table, Row, Col, Empty } from 'antd';
+import { ColumnProps } from 'antd/lib/table';
+import { PaginationConfig } from 'antd/lib/pagination';
+
+import { PageTitle } from '../MainWrapper/MainWrapper.styles';
+import { StyledTableWrapper, StyledSearch } from '../SharedStyles/Table.styles';
+
+const PAGE_SIZE = 10;
+
+type Props<RecordType> = {
+	records: RecordType[];
+	totalCount: number;
+	columns: ColumnProps<RecordType>[];
+	loading: boolean;
+	onPaginationChange: (newPageNumber: number) => void;
+	onSearch?: (searchValue: string) => void;
+	translations: {
+		search: string;
+		emptyResult: string;
+	};
+};
+
+interface Record {
+	id: string;
+}
+
+export function PaginatedTable<T extends Record>(props: Props<T>) {
+	const { t } = useTranslation();
+	const [currentPageNumber, setCurrentPageNumber] = useState(1);
+	const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+	const pagination: PaginationConfig = {
+		current: currentPageNumber,
+		total: props.totalCount,
+		pageSize: PAGE_SIZE,
+		hideOnSinglePage: true,
+		showTotal: (total, range) =>
+			t('Showing {{start}}-{{end}} of {{total}} items', {
+				total,
+				start: range[0],
+				end: range[1],
+			}),
+		onChange: (newPageNumber) => {
+			setCurrentPageNumber(newPageNumber);
+		},
+	};
+
+	const selectHandler = (id: string, isSelected: boolean) => {
+		if (isSelected) {
+			return setSelectedItems([...selectedItems, id]);
+		}
+		setSelectedItems(selectedItems.filter((currentId) => currentId !== id));
+	};
+
+	const searchSubmitHandler = (searchValue: string) => {
+		setCurrentPageNumber(1);
+		props.onSearch && props.onSearch(searchValue);
+	};
+
+	return (
+		<StyledTableWrapper>
+			{props.onSearch && (
+				<Row justify="end">
+					<Col xs={24} md={6}>
+						<StyledSearch
+							enterButton
+							placeholder={props.translations.search}
+							onSearch={searchSubmitHandler}
+						/>
+					</Col>
+				</Row>
+			)}
+			<Table<T>
+				columns={props.columns}
+				dataSource={props.records}
+				pagination={pagination}
+				loading={props.loading}
+				rowKey={(record) => record.id}
+				size="middle"
+				rowSelection={{
+					onSelect: ({ id }, isSelected) => selectHandler(id, isSelected),
+					selectedRowKeys: selectedItems,
+					onSelectAll: (_, selectedRows) => {
+						setSelectedItems(selectedRows.map(({ id }) => id));
+					},
+				}}
+				locale={{
+					emptyText: (
+						<Empty
+							image={Empty.PRESENTED_IMAGE_SIMPLE}
+							description={props.translations.emptyResult}
+						/>
+					),
+				}}
+			/>
+		</StyledTableWrapper>
+	);
+}
