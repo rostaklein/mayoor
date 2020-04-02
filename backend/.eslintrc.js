3db5bba8827baeab3c4822cdc89d0277624915e7
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-04-02 23:31:59 +0200

    eslint proper setup in both backend and frontend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        directory: './tsconfig.json',
+      },
+    },
+    'import/ignore': ['node_modules'],
+  },
+};
