a9949b5a4e819ef573f4a3638a345c2a67564e00
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-02-10 13:50:44 +0100

    forbid deleting user according to canBeDeleted flag

--- backend/src/mutations/users/deleteUser.ts
+++ backend/src/mutations/users/deleteUser.ts
@@ -1,4 +1,5 @@
 import { mutationField, idArg } from '@nexus/schema';
+import { ApolloError } from 'apollo-server-express';
 
 export const DeleteUser = mutationField('deleteUser', {
   type: 'User',
@@ -6,6 +7,12 @@ export const DeleteUser = mutationField('deleteUser', {
     id: idArg({ nullable: false }),
   },
   resolve: async (_, { id }, ctx) => {
+    const user = await ctx.prisma.user.findOne({ where: { id } });
+
+    if (!user?.canBeDeleted) {
+      throw new ApolloError('You cant delete this user.', 'INVALID_OPERATION');
+    }
+
     return ctx.prisma.user.update({
       where: { id },
       data: { deleted: true },
