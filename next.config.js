f4277489ca147ae0f433d62cbbebee1133a7f101
Rostislav Klein <rostaklein@gmail.com>
2022-09-01 22:24:27 +0200

    next config use properly TS errors

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
